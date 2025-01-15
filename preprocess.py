6580d27df1ae04d5155e8bbd62f3ff44b0bc4d31
WesScivetti <wesleyscivetti@my.unt.edu>
2025-01-15 14:08:03 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -22,6 +22,47 @@ def preprocess_conllu(input_file):
                     newline = "\t".join(newl) + "\n"
                     outf.write(newline)
 
+import stanza
+from stanza.utils.conll import CoNLL
+
+def preprocess_text_plain(input_file, lang="en"):
+    # Initialize the Stanza pipeline for the desired language
+    stanza.download(lang)
+    nlp = stanza.Pipeline(lang, processors='tokenize,mwt,pos,lemma,depparse', tokenize_no_ssplit=True)
+
+    # Process the input text with Stanza to generate CoNLL-U formatted output
+
+    with open(input_file, "r") as inf:
+        text = inf.read()
+        doc = nlp(text)
+
+    outfile_conllu = "".join(input_file.split(".")[:-1]) + ".conllu"
+
+    outfile_lex = "".join(input_file.split(".")[:-1]) + "_preprocessed.conllulex"
+
+    CoNLL.write_doc2conll(doc, outfile_conllu)
+
+    preprocess_conllu(outfile_conllu)
+
+    # outfile_name = "preprocessed_output.conllulex"
+    #
+    # with open(outfile_name, "w") as outf:
+    #     for line in conllu_lines:
+    #         if len(line) == 0:
+    #             outf.write("\n")
+    #         elif line.startswith("#"):
+    #             outf.write(line + "\n")
+    #         else:
+    #             l = line.split("\t")
+    #             # Adding extra columns
+    #             extra_stuff = ["_", "_", "_", "_", "_", "_", "_", "_", "-"]
+    #             newl = l + extra_stuff
+    #             newline = "\t".join(newl) + "\n"
+    #             outf.write(newline)
+
+    #print(f"Preprocessed CoNLL-U file with extra columns saved to {outfile_name}")
+
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -29,4 +70,7 @@ if __name__ == "__main__":
     parser.add_argument("--input_format", help="Please specify the input format of the file to preprocess. If the file is in conllu, type 'conllu'. If plain text, type 'plain'. ")
     parser.add_argument("--lang", help="specify the language (inportant for POS tagging/identifying target adpositions)")
     args = parser.parse_args()
-    preprocess_conllu(args.input_file)
+    if args.input_format == "conllu":
+        preprocess_conllu(args.input_file)
+    if args.input_format == "plain":
+        preprocess_text_plain(args.input_file, args.lang)
